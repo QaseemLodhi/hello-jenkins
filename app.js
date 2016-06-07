ccde716c0fcbd6c89ee6addaaae4228ea40449b7
QaseemLodhi <qaseemlodhi93@gmail.com>
2016-06-07 15:39:09 +0500

    connect to heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('hello jenkinsssssss');
+    res.send('hello j');
 });
 
 app.listen(process.env.PORT || 5000,function(){
