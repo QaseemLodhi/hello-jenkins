380f0294c14ff4b624bb38b530fb64cf7abba19c
QaseemLodhi <qaseemlodhi93@gmail.com>
2016-06-08 12:34:03 +0500

    modified

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('hello jojo');
+    res.send('hello jo');
 });
 
 app.listen(process.env.PORT || 5000,function(){
